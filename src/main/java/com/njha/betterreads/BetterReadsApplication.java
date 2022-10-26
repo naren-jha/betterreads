927420b97e110bd6223f057dd97bc6e4a3192a4a
Narendra Jha <njha.sde@gmail.com>
2022-10-26 23:46:26 +0530

    initial commit - app setup, user login through github

---
+++ src/main/java/com/njha/betterreads/BetterReadsApplication.java
@@ -0,0 +1,13 @@
+package com.njha.betterreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterReadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterReadsApplication.class, args);
+	}
+
+}
